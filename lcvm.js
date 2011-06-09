e4f5ee1aa04ed768e015cace958d6f8b450da8d7
bill <bill>
2011-06-09 21:21:02 +0000

    changes to make vm lazy

--- lcvm.js
+++ lcvm.js
@@ -1,60 +1,148 @@
+/*
+a (\x y . x) (c d) e
+--------------------
+
+[a,c,d,e]: ResolveVar(a), NewContext(L, 0), BindContext, NewClosedContext(C, 0), BindContext, BindVar(e), Return
+
+L[x]: NewClosedContext(L2, 1), Inherit(x), UseContext, Return
+L2[x]: UseVar(x), Return
+C[c,d]: UseVar(c), BindVar(d), Return
+
+Registers
+  PC: program counter
+  CP: context pointer
+  FP: function pointer
+  CS: context slot number (used by Inherit)
+  AP: arg pointer
+  SP: stack pointer
+
+
+Stack
+[PC1, CP1, ...]
+
+
+Instructions
+
+NewContext(addr, inherit) -- AP = new context, CS = 0
+  addr is address of code
+  inherit is # of parent slots to allocate
+
+NewClosedContext(addr, inherit) -- AP = new closed context, CS = 0
+  addr is address of code
+  inherit is # of parent slots to allocate
+  this context has no value
+
+Inherit(n) -- AP[CS++] = CP[n]
+  n is the index of the slot to inherit
+
+BindContext -- bind current function to arg and reduce it
+  if FP unbound, FP[0] = AP
+  stack[sp++] = PC, stack[sp++] = CP, CP = FP, PC = CP.addr
+
+BindVar(n) -- bind current function to var and reduce it
+  if FP unbound and var(n) is a context with a result, FP[0] = var(n).result
+  else if FP unbound, FP[0] = var(n)
+  stack[sp++] = PC, stack[sp++] = CP, CP = FP, PC = CP.addr
+
+UseVar(n) -- use var as function
+  if var(n) is a primitive value, a free variable, or an unbound context without a result, FP = var(n)
+  if var(n) is a primitive call, FP = var(n)()
+  if var(n) is a context with a result, FP = var(n).result
+  if var(n) is a bound context without a result, stack[sp++] = PC, stack[sp++] = CP, CP = var(n), PC = CP.addr
+
+UseContext -- FP = AP
+  FP = AP
+
+BindPrimitive(name) -- bind func to lazy prim call and reduce
+  FP unbound, FP[0] = newPrimCall(name, CP)
+  stack[sp++] = PC, stack[sp++] = CP, CP = FP, PC = CP.addr
+
+UsePrimitive(name) -- call primitive and use result as new function
+  FP = newPrimCall(name, CP)()
+
+Return -- CP.value = FP, CP = stack[sp--], PC = stack[sp--]
+
+
+Contexts
+
+Context: [result, addr, val0, val1, ...]
+
+*/
+
+
 LC = (function(){
     var code = []
     var labels = {}	// label -> code offset
     var stack = []
-    var vars = []
     var pc = 0
+    var cp = null
+    var ncp = null
+    var ncs = 0
     var sp = 0
-    var ct = null
     var contexts = []	// free list for contexts
     //OPCODES
-    var APPLY = 0	// APPLY: TMP = bind(stack[sp - 1], stack[sp]), stack[sp] =  ct, stack[sp - 1] = pc + 1, pc = TMP[0], ct = TMP -- ([func, arg] -> [pc, ct] with new ct and pc)
-    var APPLYN = 1	// APPLY(N): TMP = bind(stack[sp - N], stack[sp - N + 1], ...), stack[sp - N] =  ct, stack[sp - N + 1] = pc + 1, sp -= N - 2, pc = TMP[0], ct = TMP -- ([func, arg, arg, ...] -> [pc, ct] with new ct and pc)
-    var TAPPLY = 2	// TAPPLY: ct = bind(stack[sp - 1], stack[sp]), sp -= 2, pc = ct[0] -- tail call apply ([ct, pc, func, arg] -> [ct, pc] with new ct and pc)
-    var TAPPLYN = 2	// TAPPLY(N): ct = bind(stack[sp - N], stack[sp - N + 1], ...), sp -= N + 1, pc = ct[0] -- tail call apply ([ct, pc, func, arg] -> [ct, pc] with new ct and pc)
-    var VAR = 4		// VAR(offset): stack[++sp] = ct[offset]
-    var RVAR = 5	// RVAR(offset): pc = stack[sp - 1], stack[sp - 1] = ct[offset], ct = stack[sp - 1], sp-- -- return a variable
-    var LAMBDA = 6	// LAMBDA(addr,size): stack.push(context(addr, size))
-    var LAMBDACP = 7	// LAMBDACP(addr): stack.push(ct)
-    var RLAMBDA = 8	// RLAMBDA(addr,size): pc = stack[sp - 1], stack[sp - 1] = context(addr, size), ct = stack[sp - 1], sp-- -- return a closure
-    var RLAMBDACP = 9	// RLAMBDACP(addr): pc = stack[sp - 1], stack[sp - 1] = ct, ct = stack[sp - 1], sp-- -- return a closure
-    var PRIMITIVE = 10	// PRIMITIVE(function): stack[++sp] = function()
-    var RPRIMITIVE = 11	// RPRIMITIVE(function): pc = stack[sp - 1], stack[sp - 1] = function(), ct = stack[sp - 1], sp-- -- return a primitive
+    var NEW_CONTEXT = 0
+    var NEW_CLOSED_CONTEXT = 1
+    var INHERIT = 2
+    var BIND_CONTEXT = 3
+    var BIND_VAR = 4
+    var USE_VAR = 5
+    var USE_CONTEXT = 6
+    var BIND_PRIM = 7
+    var USE_PRIM = 7
+    var RETURN = 8
 
-    //CONTEXT FLAGS
-    var FREE_CONTEXT = 0
-    var REFFED_CONTEXT = 1
+    // primitive values are 31 bits
+    // normal data is 28 bit addrs (aligned on 16 byte boundaries)
 
-    //DATA TYPES -- bottom 2 bits of a ref
-    var CONTEXT = 0
-    var PRIMITVE = 1
-    var FREE_VARIABLE = 2
+    //DATA TYPES -- bottom 3 bits of data
+    var PRIMITVE = 0
+    var CONTEXT = 1
+    var FREE_VARIABLE = 3
+    var PRIM_CALL = 5
+    // nothing for value 7
+
+    //REF FLAGS -- 4th bit of a non-primitive value
+    var FREE = 0
+    var REFFED = 8
 
 /*
 check out ptmalloc3 for the native compiler http://www.malloc.de/en/index.html
 
-context: [code addr | ctx flag, bound?, var ...] -- if bound, apply throws away the argument
+values:
+primitive value: [type + value]
+free variable value: [type + ref flag + id]
+context value: [type + ref flag + addr] -> [bound?, code addr, var ...] -- if bound, apply throws away argument during apply
+application value: [type + ref flag + addr] -> [0 or value, context addr, value] -- if value == 0, application is still needed
+
+
+
 code: ... index ... opcode ... -- the compiler/linker should know how large index is
 stack: [... ct pc value] or [... ct pc func arg]
 apply: [... ct pc func arg] -> [... ct pc ct2 pc2] or [... ct pc value func arg] -> [... ct pc value ct2 pc2]
 return: [... ct pc ct2 pc2 value] -> [... ct pc value] or [... ct pc value ct2 pc2 value] -> [... ct pc func arg]
+
+
 
 example code:
 t = \x y . x
 f = \x y . y
 and = \a b . a b a
 
-main = and t t
+main = and t (and t f)
 
 --> (code is label: ops or label: [indices] ops. label can be name or name[drop], which is compiler info that indicates to use lambdacp instead of lambda)
 
-t: RLAMBDACP(t2) -- CP to throw y away
+t: RLAMBDA0(t2, 0) -- CP to throw y away
 t2[drop]: RVAR(0) -- 0 because y was thrown away
 f[drop]: RLAMBDA(f2, 0) -- 0 because it uses no parent vars
 f2: RVAR(0)
 and: RLAMBDA(and2, 1)
 and2: [0] VAR(1), VAR(0), APPLY, VAR(1), TAPPLY
-main: LAMBDA(and, 0), LAMBDA(t), APPLY, LAMBDA(t), TAPPLY
+main: LAMBDACP(and), LAMBDACP(t), APPLY, LAMBDACP(main2), TAPPLY
+main2: LAMBDACP(and), LAMBDACP(t), APPLY, LAMBDACP(f), TAPPLY
+
+A bytecode verifier can ensure that TAPPLY and R-- opcodes only appear at the end of a code body
 */
 
     for (var i = 0; i < 256; i++) contexts[i] = []
@@ -70,46 +158,13 @@ main: LAMBDA(and, 0), LAMBDA(t), APPLY, LAMBDA(t), TAPPLY
 	pc = labels[label]
 	while (pc > -1) {
 	    switch (code[pc++]) {
-	    case APPLY:     // APPLY: TMP = bind(stack[sp - 1], stack[sp]), stack[sp] =  ct, stack[sp - 1] = pc + 1, pc = TMP[0], ct = TMP -- ([func, arg] -> [pc, ct] with new ct and pc)
-		var con = stack[sp]
-		var tmp = con[con.length - 1] = stack[sp - 1]
-
-		stack[sp] = ct
-		stack[sp - 1] = pc
-		pc = tmp[0]
-		ct = tmp
-		break
-	    case APPLYN:     // APPLY(N): TMP = bind(stack[sp - N], stack[sp - N + 1], ...), stack[sp - N] =  ct, stack[sp - N + 1] = pc + 1, sp -= N - 2, pc = TMP[0], ct = TMP -- ([func, arg, arg, ...] -> [pc, ct] with new ct and pc)
-**
-	    case TAPPLY:     // TAPPLY: ct = bind(stack[sp - 1], stack[sp]), sp -= 2, pc = ct[0] -- ([ct, pc, func, arg] -> [ct, pc] with new ct and pc)
-**
-	    case TAPPLYN:    // TAPPLY(N): ct = bind(stack[sp - N], stack[sp - N + 1], ...), sp -= N + 1, pc = ct[0] -- tail call apply ([ct, pc, func, arg] -> [ct, pc] with new ct and pc)
-**
-	    case VAR:       // VAR, offset: S.push(ct[offset])
-		s[++sp] = ct[pc++]
-		break
-	    case RVAR:       // VAR(offset): pc = stack[sp - 1], stack[sp - 1] = ct[offset], ct = stack[sp - 1], sp--
-**
-	    case LAMBDA:   // LAMBDA, size, addr: context(addr)
-		var size = code[pc++]
-		var cl = contexts[size] || [] // if no context, make an array of size + 1, but in JS this is just []
-		var addr = code[pc++]
-
-		cl[0] = addr
-		size-- // skip the first one, that will be supplied by a future apply
-		while (size--) cl[size] = ct[code[addr - size] + 1] // copy relevant slots from current context
-		s[++sp] = cl
-		break
-	    case LAMBDACP:   // LAMBDACP(addr): stack.push(ct)
-**
-	    case RLAMBDA:   // LAMBDA(addr,size): pc = stack[sp - 1], stack[sp - 1] = context(addr, size), ct = stack[sp - 1], sp--
-**
-	    case RLAMBDACP:   // LAMBDACP(addr): pc = stack[sp - 1], stack[sp - 1] = ct, ct = stack[sp - 1], sp--
-**
-	    case PRIMITIVE:   // PRIMITIVE(function): stack[++sp] = function()
-**
-	    case RPRIMITIVE:  // PRIMITIVE(function): pc = stack[sp - 1], stack[sp - 1] = function(), ct = stack[sp - 1], sp--
-**
+	    case NEW_CONTEXT:
+	    case NEW_CLOSED_CONTEXT:
+	    case INHERIT:
+	    case BIND_CONTEXT:
+	    case BIND_VAR:
+	    case USE_VAR:
+	    case USE_CONTEXT:
 	    }
 	}
     }
