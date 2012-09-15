cd5378e1b33e6c93bbc641078f32f5578dbecac3
Bill Burdick <bill.burdick@gmail.com>
2012-09-14 22:35:14 -0500

    added jquery.indexeddb.js

--- bootLeisure.cs
+++ bootLeisure.cs
@@ -34,7 +34,7 @@ bootLeisure = ->
     style.setAttribute 'href', "#{i}.css"
     document.head.appendChild style
   #loadThen ['parse', 'leisure', 'prim', 'replCore', 'browserRepl', 'std', 'notebook', 'jquery-1.7.2.min', 'jqModal', 'jqDnR', 'dimensions', 'sha256'], ->
-  loadThen ['parse', 'leisure', 'prim', 'replCore', 'browserRepl', 'prelude', 'std', 'parsing', 'notebook', 'jquery-1.7.2.min', 'storage'], ->
+  loadThen ['parse', 'leisure', 'prim', 'replCore', 'browserRepl', 'prelude', 'std', 'parsing', 'notebook', 'jquery-1.7.2.min', 'jquery.indexeddb', 'storage'], ->
     window.Leisure.restoreAutosave = restoreAutosave
     window.Leisure.backupAutosave = backupAutosave
     window.Leisure.deleteAutosave = deleteAutosave
