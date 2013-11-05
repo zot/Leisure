4e6b7a9ff2262846d67892303169f32cdcc69083
Bill Burdick <bill.burdick@gmail.com>
2013-11-05 08:52:29 +0200

    moved a bunch of files to old/

--- cmd
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-cp lc.js /tmp/all.js
-echo >> /tmp/all.js
-cat lcvm.js >> /tmp/all.js
-echo >> /tmp/all.js
-cat cmd.js >> /tmp/all.js
-exec node /tmp/all.js
--- hello.c
+++
@@ -1,8 +0,0 @@
-/* to generate LLVM assembly: llvm-gcc-4.2 -O2 -emit-llvm -S hello.c */
-
-#include <stdio.h>
-
-int main() {
-	printf("hello world\n");
-	return 0;
-}
